7c374bd299eb26a5bf371c99190abd92b60a358a
Bilibilimoon <2512724542@qq.com>
2024-06-19 12:54:40 +0800

    ing

--- src/auto_control/include/Auto_control.hpp
+++ src/auto_control/include/Auto_control.hpp
@@ -6,3 +6,42 @@
 #include <cmath>
 
 #include "fpv_msgs/msg/moonly_fpv.hpp"  //我自定义的FPV消息
+
+//期望动作输出
+double rol; //横滚角
+double pit; //俯仰角
+double yaw; //偏航角
+double thr; //油门
+
+//目标位置
+double aim_gps_lng; //目标经度
+double aim_gps_lat; //目标纬度
+
+//飞机实时信息
+double fpv_gps_lng;     //飞机当前经度
+double fpv_gps_lat;     //飞机当前纬度
+double fpv_gps_hight;   //飞机当前海拔
+double fpv_gps_face;    //飞机当前水平朝向角(360°制)
+
+//订阅无人机实时信息
+void fpv_gps_subscriber_Callback(const fpv_msgs::msg::MoonlyFpv::SharedPtr msg)
+{
+    fpv_gps_face = msg->fpv_gps_face;
+    fpv_gps_hight = msg->fpv_gps_hight;
+    fpv_gps_lat = msg->fpv_gps_lat;
+    fpv_gps_lng = msg->fpv_gps_lng;
+}
+
+//订阅目标GPS位置信息
+void aim_gps_subscriber_Callback(const fpv_msgs::msg::MoonlyFpv::SharedPtr msg)
+{
+    aim_gps_lat = msg->aim_gps_lat;
+    aim_gps_lng = msg->aim_gps_lng;
+}
+
+//循环发布期望动作
+void act_pub()
+{
+    
+}
+
--- src/auto_control/src/Auto_control.cpp
+++ src/auto_control/src/Auto_control.cpp
@@ -21,8 +21,18 @@ int main(int argc, char **argv)
     //创建名为node的智能指针对象
     auto node = rclcpp::Node::make_shared("Pwm_trans"); //创建节点
 
+    //创建订阅器接收无人机GPS位置信息
+    auto fpv_gps_subscriber = node->create_subscription<fpv_msgs::msg::MoonlyFpv>("/fpv_info", 10, fpv_gps_subscriber_Callback);
+    //订阅目标GPS位置信息
+    auto aim_gps_subscriber = node->create_subscription<fpv_msgs::msg::MoonlyFpv>("/aim_gps", 10, aim_gps_subscriber_Callback);
+
+    //发布FPV的期望动作
+    auto act_pub_timer = node->create_wall_timer(std::chrono::milliseconds(50), act_pub);
+
     printf("\033[2J\033[1;1H");  //清屏
     printf("%s\n",Readme);  //输出提示消息
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(1000));   //延迟1s
 
     rclcpp::spin(node);
     rclcpp::shutdown();
